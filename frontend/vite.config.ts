d82699d09144df22de9b6d5a43f8aa0fa1ed2faf
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-02-18 22:23:52 +0900

    Set frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
+    host: true,
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://blog_backend:8080',
         changeOrigin: true,
       },
     },
