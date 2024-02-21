3c313b8e71e931ddbba38a1fc67e999884238c48
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-02-22 00:38:16 +0900

    Add Profile page

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -7,23 +7,83 @@ const Profile = () => {
         <Title id="profile">Profile</Title>
         <img src="images/marker.svg" height={'20px'} />
       </TitleWrapper>
+      <ProfileWrapper>
+        <Img src="images/profile.png" />
+        <ProfileGreeting />
+      </ProfileWrapper>
     </ProfileOuter>
   );
 };
 
 export default Profile;
 
+const ProfileGreetingOuter = styled.div`
+  display: flex;
+  flex-direction: column;
+  vertical-align: top;
+`;
+
+const ProfileGreetingTitle = styled.h1`
+  display: flex;
+  flex-direction: row;
+  font-weight: bold;
+  font-size: 20px;
+  color: var(--white);
+`;
+const ProfileGreetingBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ProfileGreetingBodyElement = styled.p`
+  color: var(--white);
+`;
+
+const ProfileGreeting = () => {
+  return (
+    <ProfileGreetingOuter>
+      <ProfileGreetingTitle>I'm Kaito Kimura</ProfileGreetingTitle>
+      <ProfileGreetingBody>
+        <ProfileGreetingBodyElement>
+          I am passionate about UI/UX design and Web Design. I am a skilled
+          front-end developer and master of graphic design tools such as
+          Photoshop and Sketch. I am a quick learner and a team worker that gets
+          the job done. I can easily capitalize on low hanging fruits and
+          quickly maximize timely deliverables for real-time schemas.
+        </ProfileGreetingBodyElement>
+      </ProfileGreetingBody>
+    </ProfileGreetingOuter>
+  );
+};
+
 const ProfileOuter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding-top: 100px;
   height: 400px;
   width: 1000px;
   background-color: var(--light-black-background-color);
 `;
 
+const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const Img = styled.img`
+  height: 200px;
+  width: 200px;
+  border-radius: 20px;
+`;
+
 const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  width: 600px;
 `;
 
 const Title = styled.div`
