9da49bb569576cdd1f8c2dd2a28defc82e1c0687
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-02-22 00:13:20 +0900

    Add AboutHeader

---
+++ frontend/src/components/AboutHeader.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+const AboutHeader = () => {
+  return (
+    <AboutHeaderOuter>
+      <AboutHeaderWrapper>
+        <Greeting />
+        <Img src="images/man-using-pc.svg" />
+      </AboutHeaderWrapper>
+      <Img2 src="images/wave-bg.svg" />
+    </AboutHeaderOuter>
+  );
+};
+
+export default AboutHeader;
+
+const AboutHeaderOuter = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  width: 1000px;
+`;
+
+const AboutHeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 1000px;
+`;
+
+const Img = styled.img`
+  background-color: var(--black-background-color);
+  height: 300px;
+  width: 300px;
+`;
+
+const Img2 = styled.img`
+  width: 1000px;
+  z-index: 1000;
+`;
+
+const Greeting = () => {
+  return (
+    <GreetingOuter>
+      <H2>Hello World</H2>
+      <H1>I'm Kaito Kimura</H1>
+      <H2>College Student and Web Engineer</H2>
+    </GreetingOuter>
+  );
+};
+
+const GreetingOuter = styled.div`
+  background-color: var(--black-background-color);
+  display: flex;
+  flex-direction: column;
+`;
+
+const H1 = styled.h1`
+  font-size: 35px;
+  font-weight: bold;
+`;
+
+const H2 = styled.h2`
+  font-size: 20px;
+  font-weight: 400;
+`;
