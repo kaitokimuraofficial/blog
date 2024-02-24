ea7fb10c51a6def09b599746389f13f8d5cab0ff
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-02-24 14:17:26 +0900

    Fix career Component

--- frontend/src/components/About/Career.tsx
+++ frontend/src/components/About/Career.tsx
@@ -6,30 +6,35 @@ import { AboutOuterLayout } from 'cmp/shared';
 
 const CareerBody = () => {
   return (
-    <CareerBodyContent>
+    <BodyWrapper>
+      <HighSchool />
       <Waseda />
       <Torotaku />
       <Qiita />
-    </CareerBodyContent>
+    </BodyWrapper>
   );
 };
 
+const BodyWrapper = styled.div`
+  align-items: flex-start;
+  display: grid;
+  grid-template-columns: 20rem 20rem;
+  grid-row: auto auto;
+  grid-column-gap: 4.5rem;
+  grid-row-gap: 4.5rem;
+  justify-items: center;
+`;
+
 const props = {
   content: <CareerBody />,
-  height: 600,
+  height: 700,
+  jc: 'center',
   title: 'Career',
 };
 
 export function Career() {
   return <AboutOuterLayout {...props} />;
 }
-
-const CareerBodyContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 type ElementProps = {
   title: string;
@@ -50,27 +55,31 @@ const Element = ({ title, icon, body }: ElementProps) => {
 };
 
 const ElementOuter = styled.div`
+  align-items: center;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  gap: 2.5rem;
+  justify-content: space-between;
+  
 `;
 
 const ElementHeader = styled.div`
   display: flex;
   flex-direction: row;
+  gap: 1rem;
   justify-content: space-between;
   vertical-align: middle;
 `;
 
 const HeaderTitle = styled.h2`
-  font-size: 25px;
-  font-weight: bold;
   color: var(--indigo);
+  font-size: 1.5rem;
+  font-weight: bold;
 `;
 
 const Img = styled.img`
-  height: 20px;
-  width: 20px;
+  height: 2rem;
+  width: 2rem;
 `;
 
 const ElementBody = styled.div`
@@ -79,35 +88,31 @@ const ElementBody = styled.div`
   padding: 3px;
 `;
 
-const wasedaProp = {
-  title: 'Waseda University',
+const highSchoolProp = {
+  title: '東京都立西高等学校',
   icon: 'images/icons/college.svg',
-  body: 'Leverage agile frameworks to provide a robust \
-  synopsis for high level overviews. Iterative approaches \
-  to corporate strategy foster collaborative thinking to \
-  further the overall value proposition.',
+  body: 'バスケットボール部所属運動会実行委員会\nクラスマッチ実行委員会\n運動会総団長\n文化祭での映像制作にて文化祭大賞受賞\n',
 };
 
-const Waseda = () => Element(wasedaProp);
+const wasedaProp = {
+  title: '早稲田大学 基幹理工学部 情報理工学科',
+  icon: 'images/icons/college.svg',
+  body: '「プロジェクト研究」制度を利用して、三年次から寺内研究室にて活動をしております。具体的な研究にはまだ着手できていませんが、現在はプログラムにおける型理論による検証や型推論の学習を通じて4年次の研究の知識の土台を作っております。',
+};
 
 const torotakuProp = {
-  title: 'Waseda University',
-  icon: 'images/icons/college.svg',
-  body: 'Leverage agile frameworks to provide a robust \
-  synopsis for high level overviews. Iterative approaches \
-  to corporate strategy foster collaborative thinking to \
-  further the overall value proposition.',
+  title: '株式会社とろたく',
+  icon: 'images/icons/display.svg',
+  body: 'デスクトップでゲームをプレイしている時に表示される英語字幕をOCR技術によって読み取り日本語に翻訳するデスクトップアプリケーションをC#にて開発するプロジェクトにジョインしました。また、HTML・CSSを用いたサイト制作の業務にも携わりました。',
 };
-
-const Torotaku = () => Element(torotakuProp);
 
 const qiitaProp = {
-  title: 'Waseda University',
-  icon: 'images/icons/college.svg',
-  body: 'Leverage agile frameworks to provide a robust \
-  synopsis for high level overviews. Iterative approaches \
-  to corporate strategy foster collaborative thinking to \
-  further the overall value proposition.',
+  title: 'Qiita',
+  icon: 'images/icons/display2.svg',
+  body: '「エンジニアに関する知識を記録・共有するサービス」であるQiitaの開発業務にジョインしております。Ruby・TypeScriptを用いてフロントエンド・バックエンドをまたいだ保守・新機能追加のタスクを通じて日々成長しております。',
 };
 
+const HighSchool = () => Element(highSchoolProp);
+const Waseda = () => Element(wasedaProp);
+const Torotaku = () => Element(torotakuProp);
 const Qiita = () => Element(qiitaProp);
