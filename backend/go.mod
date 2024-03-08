207c4844bae0c671cb1caad52ec87e7b9c334e7b
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-03-08 22:00:52 +0900

    Fix main.go

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,10 @@ module blog
 go 1.22.0
 
 require (
+	github.com/go-chi/chi v1.5.5
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/jmoiron/sqlx v1.3.5
+	golang.org/x/sync v0.6.0
 )
+
+require github.com/go-chi/chi/v5 v5.0.12
--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-	"blog/controller"
-	"net/http"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	mux := controller.GetRouter()
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		return
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
 	}
+}
+
+func run(ctx context.Context) error {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", 8080))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", 8080, err)
+	}
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
+	mux, err := NewMux(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
