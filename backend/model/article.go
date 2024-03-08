3ef711917c0774084892b5cc3e009230f0829994
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-03-08 22:00:39 +0900

    Fix Model and introduced of article

--- backend/model/article.go
+++ backend/model/article.go
@@ -4,10 +4,12 @@ import (
 	"time"
 )
 
+type ArticleID int64
+
 type Article struct {
-	ArticleID int
-	Title     string
-	Body      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        ArticleID `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	Body      string    `json:"body" db:"body"`
+	CreatedAt time.Time `json:"created" db:"created"`
+	UpdatedAt time.Time `json:"modified" db:"modified"`
 }
