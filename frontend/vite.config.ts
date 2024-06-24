22e89cc867e5cf731f619f6b3d92cc9412858dd0
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-06-24 16:25:18 +0900

    Change some settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   const plugins = [react()];
@@ -13,7 +13,7 @@ export default defineConfig(({ command, mode }) => {
     plugins,
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        src: path.resolve(__dirname, './src'),
         img: path.resolve(__dirname, 'src/images'),
       },
     },
