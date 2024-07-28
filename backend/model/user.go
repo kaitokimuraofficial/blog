0307591e22f9ce100d6ebe0e2881cc93edde1322
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-07-28 18:44:10 +0900

    Fix format of User model

--- backend/model/user.go
+++ backend/model/user.go
@@ -7,9 +7,9 @@ import (
 type UserID int64
 
 type User struct {
-	ID        UserID `json:"userId" db:"UserId"`
-	Name     string    `json:"name" db:"name"`
-	Email      string    `json:"email" db:"email"`
+	ID        UserID    `json:"userId" db:"UserId"`
+	Name      string    `json:"name" db:"name"`
+	Email     string    `json:"email" db:"email"`
 	CreatedAt time.Time `json:"created" db:"createdAt"`
 	UpdatedAt time.Time `json:"updated" db:"UpdatedAt"`
 }
