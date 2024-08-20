82c376a18719a9a52df38573cf0d7234c28480c0
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-20 16:37:24 +0900

    Add env for build

--- backend/config/config.go
+++ backend/config/config.go
@@ -2,7 +2,11 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -19,9 +23,60 @@ type Config struct {
 
 func New() (*Config, error) {
 	cfg := &Config{}
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+
 	if err := env.Parse(cfg); err != nil {
 		return nil, fmt.Errorf("failed to parsing config: %w", err)
 	}
+
+	if env := os.Getenv("ENV"); env != "" {
+		cfg.Env = env
+	}
+
+	if dbHost := os.Getenv("BLOG_DB_HOST"); dbHost != "" {
+		cfg.DBHost = dbHost
+	}
+
+	if dbPort := os.Getenv("BLOG_DB_PORT"); dbPort != "" {
+		if port, err := strconv.Atoi(dbPort); err == nil {
+			cfg.DBPort = port
+		} else {
+			return nil, fmt.Errorf("invalid port value for BLOG_DB_PORT: %s", dbPort)
+		}
+	}
+
+	if dbUser := os.Getenv("BLOG_DB_USER"); dbUser != "" {
+		cfg.DBUser = dbUser
+	}
+
+	if dbPassword := os.Getenv("BLOG_DB_PASSWORD"); dbPassword != "" {
+		cfg.DBPassword = dbPassword
+	}
+
+	if dbName := os.Getenv("BLOG_DB_NAME"); dbName != "" {
+		cfg.DBName = dbName
+	}
+
+	if parseTime := os.Getenv("BLOG_PARSE_TIME"); parseTime != "" {
+		if parse, err := strconv.ParseBool(parseTime); err == nil {
+			cfg.ParseTime = parse
+		} else {
+			return nil, fmt.Errorf("invalid boolean value for BLOG_PARSE_TIME: %s", parseTime)
+		}
+	}
+
+	if bePort := os.Getenv("BLOG_BE_PORT"); bePort != "" {
+		if port, err := strconv.Atoi(bePort); err == nil {
+			cfg.BEPort = port
+		} else {
+			return nil, fmt.Errorf("invalid port value for BLOG_BE_PORT: %s", bePort)
+		}
+	}
+
 
 	return cfg, nil
 }
--- backend/go.mod
+++ backend/go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
